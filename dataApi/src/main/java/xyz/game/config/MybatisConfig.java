78b96ddfc1bb00253a784d011904bf0da1675bc4
ml <haveno@qq.com>
2025-01-28 15:52:04 +0800

    feat: 选择schema写法优化

--- dataApi/src/main/java/xyz/game/config/MybatisConfig.java
+++ dataApi/src/main/java/xyz/game/config/MybatisConfig.java
@@ -1,7 +1,12 @@
 package xyz.game.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.jsf.el.SpringBeanFacesELResolver;
+import xyz.game.dao.SchemaDao;
 
 import java.util.Map;
 import java.util.function.Consumer;
--- dataApi/src/main/java/xyz/game/dao/SchemaDao.java
+++ dataApi/src/main/java/xyz/game/dao/SchemaDao.java
@@ -1,5 +1,7 @@
 package xyz.game.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 /**
  * 属性表(Attribute)表数据库访问层
  *
@@ -7,6 +9,6 @@ package xyz.game.dao;
  * @since 2024-06-23 21:23:34
  */
 public interface SchemaDao {
-    void chooseSchema();
+    void chooseSchema(@Param("currentSchema") String currentSchema);
 }
 
